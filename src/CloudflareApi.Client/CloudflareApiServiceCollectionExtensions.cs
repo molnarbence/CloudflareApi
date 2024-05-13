1c0b3931ae639b0017a0e672023a3dc1ca6dcf5f
Bence Molnár <xjarekx@gmail.com>
2024-05-13 21:57:38 +0200

    Ability to configure http client builder

--- src/CloudflareApi.Client/CloudflareApiServiceCollectionExtensions.cs
+++ src/CloudflareApi.Client/CloudflareApiServiceCollectionExtensions.cs
@@ -7,19 +7,27 @@ namespace Microsft.Extensions.DependencyInjection;
 public static class CloudflareApiServiceCollectionExtensions
 {
    private const string ApiBaseAddress = "https://api.cloudflare.com/client/v4";
-   public static IServiceCollection AddCloudflareApi(this IServiceCollection services, IConfiguration configuration)
+
+   public static IServiceCollection AddCloudflareApi(this IServiceCollection services, IConfiguration configuration) => services.AddCloudflareApi(configuration, null);
+
+   public static IServiceCollection AddCloudflareApi(this IServiceCollection services, IConfiguration configuration, Action<IHttpClientBuilder>? httpClientBuilder)
    {
       services
          .AddTransient<CloudflareAuthorizationDelegatingHandler>()
-         .AddRefitClient<IZonesApi>()
-         .ConfigureHttpClient(client => client.BaseAddress = new Uri($"{ApiBaseAddress}/zones"))
-         .AddHttpMessageHandler<CloudflareAuthorizationDelegatingHandler>();
-
-      services
          .AddOptions<CloudflareConfiguration>()
          .Bind(configuration.GetSection(CloudflareConfiguration.SectionName))
          .ValidateDataAnnotations()
          .ValidateOnStart();
+
+      var zonesApiBuilder = services
+         .AddRefitClient<IZonesApi>()
+         .ConfigureHttpClient(client => client.BaseAddress = new Uri($"{ApiBaseAddress}/zones"))
+         .AddHttpMessageHandler<CloudflareAuthorizationDelegatingHandler>();
+
+      if (httpClientBuilder is not null)
+      {
+         httpClientBuilder(zonesApiBuilder);
+      }
 
       return services;
    }
